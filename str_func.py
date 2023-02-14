1aeeb5620fdb926a08aa7da6451b648bde816038
mishustik-basket333 <iiiskhakov1990@gmail.com>
2023-02-14 16:12:11 +0300

    add func_title in str_func.py

--- str_func.py
+++ str_func.py
@@ -4,3 +4,11 @@ def funct_upper(str_in):
     и возвращает его же в верхнем регистре
     """
     return(str_in.upper())
+
+def funct_title(str_in):
+    """
+    Принимает строковый аргумент и возврщает его с заглавными буквами
+    :param str_in:
+    :return: str_in.title()
+    """
+    return str_in.title()
